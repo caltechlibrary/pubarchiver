4db2f5a84692ba0efd66071698893061cca7f6b9
Tom Morrell <tmorrell@caltech.edu>
2018-12-18 12:58:42 -0800

    Complete but not tested version

--- package_portico.py
+++ package_portico.py
@@ -1,4 +1,4 @@
-import requests, sys, base64, glob,subprocess
+import requests, sys, base64, glob, subprocess, datetime, os, shutil
 import xmltodict
 
 def validate_response(response):
@@ -15,15 +15,48 @@ def trace_url(url):
 endpoint = 'https://api.datacite.org/works/'
 
 files = glob.glob('*.pdf')
+if os.path.isdir('data') == True:
+    print("Please remvove the old data directory")
+    exit()        
+os.mkdir('data')
+os.chdir('data')
 
 for f in files:
     doi_parts = f[0:-4].split('-')
-    doi = doi_parts[0] +'/'+doi_parts[1]
+    prefix = doi_parts[0]
+    identifier = doi_parts[1]
+    doi = prefix +'/'+ identifier
     query = endpoint + doi
     response = requests.get(query)
     response = validate_response(response)
     xml = base64.b64decode(response['data']['attributes']['xml'])
-    metadata = xmltodict.parse(xml)
+    mfile = xmltodict.parse(xml)
 
-    print(metadata)
+    os.mkdir(identifier)    
+    os.chdir(identifier)
+    os.mv('../'+f,identifier+'.pdf')
+    
+    issn = '2578-9430'
 
+    volume = mfile['resource']['publicationYear']-2014
+    mfile['resource']['volume'] = volume
+    mfile['resource']['journal'] = mfile['resource'].pop('publisher')
+    mfile['resource']['e-issn'] = issn
+    mfile['resource']['file'] = identifier+'.pdf'
+    mfile['resource']["rightsList"] = [{
+            "rights": "Creative Commons Attribution 4.0",
+            "rightsURI": "https://creativecommons.org/licenses/by/4.0/legalcode"}] 
+    outname = identifier+'.xml'
+    outstring = xmltodict.unparse(mfile)
+    outfile = open(outname,'w',encoding='utf8')
+    outfile.write(outstring)
+    
+    os.chdir('..')
+
+    
+datestamp = datetime.tate.today().isoformat()
+print(datestamp)
+
+os.chdir('..')
+
+shutil.make_archive(issn+'_'+datestamp+'.zip', 'zip','data/')
