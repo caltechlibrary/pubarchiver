4b0969675a8d227ce36b181fd34de090cda7fc0a
Michael Hucka <mhucka@caltech.edu>
2019-06-11 15:45:05 -0700

    Add __init__ and __version__ files

---
+++ microarchiver/__init__.py
@@ -0,0 +1,21 @@
+'''
+Microarchiver -- Package up microPublications for archiving in Portico
+
+Authors
+-------
+
+Tom Morrell <tmorrell@caltech.edu> -- Caltech Library
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from .__version__ import __version__, __title__, __name__
+from .__version__ import __description__, __url__
+from .__version__ import __author__, __email__
+from .__version__ import __license__, __copyright__
---
+++ microarchiver/__version__.py
@@ -0,0 +1,18 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Microarchiver version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/martian
+# =============================================================================
+
+__version__     = '0.0.1'
+__title__       = 'microarchiver'
+__name__        = 'Microarchiver'
+__description__ = '''Create archives of articles from microPublication.org for sending to Portico.'''
+__url__         = 'https://github.com/caltechlibrary/microarchiver'
+__author__      = 'Tom Morrell <tmorrell@caltech.edu>, Michael Hucka <mhucka@caltech.edu>'
+__credits__     = ['Tom Morrell', 'Michael Hucka']
+__email__       = 'dld@library.caltech.edu'
+__license__     = 'BSD 3-clause license -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2019 by the California Institute of Technology'
