9f781e34a128428ef6340b082f75ed50df6e8c00
Michael Hucka <mhucka@caltech.edu>
2019-06-12 09:06:37 -0700

    Add custom exceptions

---
+++ microarchiver/exceptions.py
@@ -0,0 +1,38 @@
+'''
+exceptions.py: exceptions defined by Microarchiver
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+class UserCancelled(Exception):
+    '''The user elected to cancel/quit the program.'''
+    pass
+
+class ServiceFailure(Exception):
+    '''Unrecoverable problem involving network services.'''
+    pass
+
+class NoContent(Exception):
+    '''Server returned a code 401 or 404, indicating no content found.'''
+
+class RateLimitExceeded(Exception):
+    '''The service flagged reports that its rate limits have been exceeded.'''
+    pass
+
+class InternalError(Exception):
+    '''Unrecoverable problem involving Microarchiver itself.'''
+    pass
+
+class RequestError(Exception):
+    '''Problem with the TIND query or request.'''
+    pass
