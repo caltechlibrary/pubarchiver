f64d2958498199d5a34f51bdc1a4c741e2fdccf1
Michael Hucka <mhucka@caltech.edu>
2019-06-12 09:13:13 -0700

    Add a version of my common files.py

---
+++ microarchiver/files.py
@@ -0,0 +1,164 @@
+'''
+files.py: utilities for working with files.
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+import os
+from   os import path
+import sys
+import subprocess
+import webbrowser
+
+import microarchiver
+from microarchiver.debug import log
+
+
+# Constants.
+# .............................................................................
+
+_APP_NAME = 'Microarchiver'
+'''The human name of this application for human consumption.'''
+
+_APP_REG_PATH = r'Software\Caltech Library\Microarchiver\Settings'
+'''The Windows registry path for this application.'''
+
+
+# Main functions.
+# .............................................................................
+
+def readable(dest):
+    '''Returns True if the given 'dest' is accessible and readable.'''
+    return os.access(dest, os.F_OK | os.R_OK)
+
+
+def writable(dest):
+    '''Returns True if the destination is writable.'''
+    return os.access(dest, os.F_OK | os.W_OK)
+
+
+def module_path():
+    '''Returns the absolute path to our module installation directory.'''
+    # The path returned by module.__path__ is to the directory containing
+    # the __init__.py file.
+    return path.abspath(microarchiver.__path__[0])
+
+
+def installation_path():
+    '''Returns the path to where the application is installed.'''
+    # The path returned by module.__path__ is to the directory containing
+    # the __init__.py file.  What we want here is the path to the installation
+    # of the application binary.
+    if sys.platform.startswith('win'):
+        from winreg import OpenKey, CloseKey, QueryValueEx, HKEY_LOCAL_MACHINE, KEY_READ
+        try:
+            if __debug__: log('reading Windows registry entry')
+            key = OpenKey(HKEY_LOCAL_MACHINE, _APP_REG_PATH)
+            value, regtype = QueryValueEx(key, 'Path')
+            CloseKey(key)
+            if __debug__: log('path to windows installation: {}'.format(value))
+            return value
+        except WindowsError:
+            # Kind of a problem. Punt and return a default value.
+            return path.abspath('C:\Program Files\{}'.format(_APP_NAME))
+    else:
+        return path.abspath(path.join(module_path(), '..'))
+
+
+def desktop_path():
+    '''Returns the path to the user's desktop directory.'''
+    if sys.platform.startswith('win'):
+        return path.join(path.join(os.environ['USERPROFILE']), 'Desktop')
+    else:
+        return path.join(path.join(path.expanduser('~')), 'Desktop')
+
+
+def datadir_path():
+    '''Returns the path to Lost It's internal data directory.'''
+    return path.join(module_path(), 'data')
+
+
+def rename_existing(file):
+    '''Renames 'file' to 'file.bak'.'''
+
+    def rename(f):
+        backup = f + '.bak'
+        # If we fail, we just give up instead of throwing an exception.
+        try:
+            os.rename(f, backup)
+            if __debug__: log('renamed {} to {}', file, backup)
+        except:
+            try:
+                delete_existing(backup)
+                os.rename(f, backup)
+            except:
+                if __debug__: log('failed to delete {}', backup)
+                if __debug__: log('failed to rename {} to {}', file, backup)
+
+    if path.exists(file):
+        rename(file)
+        return
+    full_path = path.join(os.getcwd(), file)
+    if path.exists(full_path):
+        rename(full_path)
+        return
+
+
+def delete_existing(file):
+    '''Delete the given file.'''
+    # Check if it's actually a directory.
+    if path.isdir(file):
+        if __debug__: log('doing rmtree on directory {}', file)
+        try:
+            shutil.rmtree(file)
+        except:
+            if __debug__: log('unable to rmtree {}; will try renaming', file)
+            try:
+                rename_existing(file)
+            except:
+                if __debug__: log('unable to rmtree or rename {}', file)
+    else:
+        if __debug__: log('doing os.remove on file {}', file)
+        os.remove(file)
+
+
+def file_in_use(file):
+    if not path.exists(file):
+        return False
+    if sys.platform.startswith('win'):
+        # This is a hack, and it really only works for this purpose on Windows.
+        try:
+            os.rename(file, file)
+            return False
+        except:
+            return True
+    return False
+
+
+def open_file(file):
+    '''Open document with default application in Python.'''
+    # Code originally from https://stackoverflow.com/a/435669/743730
+    if __debug__: log('opening file {}', file)
+    if sys.platform.startswith('darwin'):
+        subprocess.call(('open', file))
+    elif os.name == 'nt':
+        os.startfile(file)
+    elif os.name == 'posix':
+        subprocess.call(('xdg-open', file))
+
+
+def open_url(url):
+    '''Open the given 'url' in a web browser using the current platform's
+    default approach.'''
+    if __debug__: log('opening url {}', url)
+    webbrowser.open(url)
