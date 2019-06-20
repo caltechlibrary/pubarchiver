5a3faa108cb3b4475fa02a0f8d18e31736a6a8e0
Michael Hucka <mhucka@caltech.edu>
2019-06-20 15:07:16 -0700

    Add code for creating archives (taken from eprints2bags)

--- microarchiver/files.py
+++ microarchiver/files.py
@@ -16,9 +16,13 @@ file "LICENSE" for more information.
 
 import os
 from   os import path
-import sys
+import shutil
 import subprocess
+import sys
+import tarfile
 import webbrowser
+import zipfile
+from   zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
 
 import microarchiver
 from microarchiver.debug import log
@@ -173,3 +177,49 @@ def make_dir(dir_path):
         if __debug__: log('Creating directory {}', dir_path)
         # If this gets an exception, let it bubble up to caller.
         os.makedirs(dir_path)
+
+
+def create_archive(archive_file, type, source_dir, comment = None):
+    root_dir = path.dirname(path.normpath(source_dir))
+    base_dir = path.basename(source_dir)
+    if type.endswith('zip'):
+        format = ZIP_STORED if type.startswith('uncompress') else ZIP_DEFLATED
+        current_dir = os.getcwd()
+        try:
+            os.chdir(root_dir)
+            with zipfile.ZipFile(archive_file, 'w', format) as zf:
+                for root, dirs, files in os.walk(base_dir):
+                    for file in files:
+                        zf.write(os.path.join(root, file))
+                if comment:
+                    zf.comment = comment.encode()
+        finally:
+            os.chdir(current_dir)
+    else:
+        if type.startswith('uncompress'):
+            shutil.make_archive(source_dir, 'tar', root_dir, base_dir)
+        else:
+            shutil.make_archive(source_dir, 'gztar', root_dir, base_dir)
+
+
+def verify_archive(archive_file, type):
+    '''Check the integrity of an archive and raise an exception if needed.'''
+    if type.endswith('zip'):
+        error = ZipFile(archive_file).testzip()
+        if error:
+            raise CorruptedContent('Failed to verify file "{}"'.format(archive_file))
+    else:
+        # Algorithm originally from https://stackoverflow.com/a/32312857/743730
+        tfile = None
+        try:
+            tfile = tarfile.open(archive_file)
+            for member in tfile.getmembers():
+                content = tfile.extractfile(member.name)
+                if content:
+                    for chunk in iter(lambda: content.read(1024), b''):
+                        pass
+        except Exception as ex:
+            raise CorruptedContent('Failed to verify file "{}"'.format(archive_file))
+        finally:
+            if tfile:
+                tfile.close()
