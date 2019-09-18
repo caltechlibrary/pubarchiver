6e0e67f814df5f5071e58a66f02a5cb8faad01bb
Michael Hucka <mhucka@caltech.edu>
2019-09-18 14:27:50 -0700

    Remove __version__.py in favor of using new setup.cfg code

--- microarchiver/__init__.py
+++ microarchiver/__init__.py
@@ -15,7 +15,28 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from .__version__ import __version__, __title__, __name__
-from .__version__ import __description__, __url__
-from .__version__ import __author__, __email__
-from .__version__ import __license__, __copyright__
+import os
+from   os import path
+from   setuptools.config import read_configuration
+import sys
+
+# Set module-level dunder variables like __version__
+# .............................................................................
+# This code reads metadata values from ../setup.cfg and sets the corresponding
+# module-level variables with '__' surrounding their names.  This approach
+# avoids putting version and other info in more than one place.
+
+keys = ['version', 'description', 'license', 'url', 'keywords',
+        'author', 'author_email', 'maintainer', 'maintainer_email']
+
+try:
+    here        = path.abspath(path.dirname(__file__))
+    conf_dict   = read_configuration(path.join(here, '..', 'setup.cfg'))
+    conf        = conf_dict['metadata']
+    this_module = sys.modules[__package__]
+    for key in keys:
+        if key in conf:
+            variable_name = '__' + key + '__'
+            setattr(this_module, variable_name, str(conf[key]))
+except:
+    pass
