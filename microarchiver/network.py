955a2ed000be34c0a2e7529c0f0a305346789194
Michael Hucka <mhucka@caltech.edu>
2019-11-21 11:24:14 -0800

    Update to newer version of this common network module

--- microarchiver/network.py
+++ microarchiver/network.py
@@ -9,8 +9,8 @@ Michael Hucka <mhucka@caltech.edu> -- Caltech Library
 Copyright
 ---------
 
-Copyright (c) 2018 by the California Institute of Technology.  This code is
-open-source software released under a 3-clause BSD license.  Please see the
+Copyright (c) 2018-2019 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
@@ -19,6 +19,7 @@ from   http.client import responses as http_responses
 import requests
 from   requests.packages.urllib3.exceptions import InsecureRequestWarning
 from   time import sleep
+import socket
 import ssl
 import urllib
 from   urllib import request
@@ -37,7 +38,7 @@ _MAX_RECURSIVE_CALLS = 10
 encountering a network error before they stop and give up.'''
 
 _MAX_FAILURES = 3
-'''Maximum number of network failures before we give up.'''
+'''Maximum number of consecutive failures before pause and try another round.'''
 
 _MAX_RETRIES = 5
 '''Maximum number of times we back off and try again.  This also affects the
@@ -47,17 +48,23 @@ maximum wait time that will be reached after repeated retries.'''
 # Main functions.
 # .............................................................................
 
-def network_available():
-    '''Return True if it appears we have a network connection, False if not.'''
-    r = None
+def network_available(address = "8.8.8.8", port = 53, timeout = 5):
+    '''Return True if it appears we have a network connection, False if not.
+    By default, this attempts to contact one of the Google DNS servers (as a
+    plain TCP connection, not as an actual DNS lookup).  Argument 'address'
+    and 'port' can be used to test a different server address and port.  The
+    socket connection is attempted for 'timeout' seconds.
+    '''
+    # Portions of this code are based on the answer by user "7h3rAm" posted to
+    # Stack Overflow here: https://stackoverflow.com/a/33117579/743730
     try:
-        r = urllib.request.urlopen("http://www.google.com")
+        if __debug__: log('testing if we have a network connection')
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((address, port))
         return True
     except Exception:
-        if __debug__: log('could not connect to http://www.google.com')
+        if __debug__: log('could not connect to https://www.google.com')
         return False
-    if r:
-        r.close()
 
 
 def timed_request(get_or_post, url, session = None, timeout = 20, **kwargs):
@@ -87,20 +94,20 @@ def timed_request(get_or_post, url, session = None, timeout = 20, **kwargs):
                 return response
         except Exception as ex:
             # Problem might be transient.  Don't quit right away.
-            if __debug__: log('exception: {}', str(ex))
             failures += 1
+            if __debug__: log('exception (failure #{}): {}', failures, str(ex))
             # Record the first error we get, not the subsequent ones, because
             # in the case of network outages, the subsequent ones will be
             # about being unable to reconnect and not the original problem.
             if not error:
                 error = ex
         if failures >= _MAX_FAILURES:
-            # Try pause & continue, in case of transient network issues.
+            # Pause with exponential back-off, reset failure count & try again.
             if retries < _MAX_RETRIES:
                 retries += 1
-                if __debug__: log('pausing because of consecutive failures')
-                sleep(60 * retries)
                 failures = 0
+                if __debug__: log('pausing because of consecutive failures')
+                sleep(10 * retries * retries)
             else:
                 # We've already paused & restarted once.
                 raise error
@@ -119,51 +126,64 @@ def net(get_or_post, url, session = None, polling = False, recursing = 0, **kwar
 
     If keyword 'polling' is True, certain statuses like 404 are ignored and
     the response is returned; otherwise, they are considered errors.
+
+    This method hands allow_redirects = True to the underlying Python requests
+    network call.
     '''
     def addurl(text):
         return (text + ' for {}').format(url)
 
     req = None
     try:
-        req = timed_request(get_or_post, url, session, **kwargs)
+        req = timed_request(get_or_post, url, session, allow_redirects = True, **kwargs)
     except requests.exceptions.ConnectionError as ex:
+        if __debug__: log('got network exception: {}', str(ex))
         if recursing >= _MAX_RECURSIVE_CALLS:
+            if __debug__: log('returning NetworkFailure')
             return (req, NetworkFailure(addurl('Too many connection errors')))
         arg0 = ex.args[0]
         if isinstance(arg0, urllib3.exceptions.MaxRetryError):
             if __debug__: log(str(arg0))
             original = unwrapped_urllib3_exception(arg0)
+            if __debug__: log('returning NetworkFailure')
             if isinstance(original, str) and 'unreacheable' in original:
                 return (req, NetworkFailure(addurl('Unable to connect to server')))
             elif network_available():
-                raise NetworkFailure(addurl('Unable to resolve host'))
+                return (req, NetworkFailure(addurl('Unable to resolve host')))
             else:
-                raise NetworkFailure(addurl('Lost network connection with server'))
+                return (req, NetworkFailure(addurl('Lost network connection with server')))
         elif (isinstance(arg0, urllib3.exceptions.ProtocolError)
               and arg0.args and isinstance(args0.args[1], ConnectionResetError)):
             if __debug__: log('net() got ConnectionResetError; will recurse')
             sleep(1)                    # Sleep a short time and try again.
+            if __debug__: log('doing recursive call #{}', recursing + 1)
             return net(get_or_post, url, session, polling, recursing + 1, **kwargs)
         else:
+            if __debug__: log('returning NetworkFailure')
             return (req, NetworkFailure(str(ex)))
     except requests.exceptions.ReadTimeout as ex:
         if network_available():
+            if __debug__: log('returning ServiceFailure')
             return (req, ServiceFailure(addurl('Timed out reading data from server')))
         else:
+            if __debug__: log('returning NetworkFailure')
             return (req, NetworkFailure(addurl('Timed out reading data over network')))
     except requests.exceptions.InvalidSchema as ex:
+        if __debug__: log('returning NetworkFailure')
         return (req, NetworkFailure(addurl('Unsupported network protocol')))
     except Exception as ex:
+        if __debug__: log('returning exception')
         return (req, ex)
 
     # Interpret the response.  Note that the requests library handles code 301
     # and 302 redirects automatically, so we don't need to do it here.
     code = req.status_code
     error = None
+    if __debug__: log(addurl('got http status code {}'.format(code)))
     if code == 400:
-        error = RequestError('Server rejected the request')
+        error = RequestError(addurl('Server rejected the request'))
     elif code in [401, 402, 403, 407, 451, 511]:
-        error = AuthenticationFailure(addurl('Access is forbidden'))
+        error = AuthFailure(addurl('Access is forbidden'))
     elif code in [404, 410] and not polling:
         error = NoContent(addurl("No content found"))
     elif code in [405, 406, 409, 411, 412, 414, 417, 428, 431, 505, 510]:
@@ -175,15 +195,31 @@ def net(get_or_post, url, session = None, polling = False, recursing = 0, **kwar
             pause = 5 * (recursing + 1)   # +1 b/c we start with recursing = 0.
             if __debug__: log('rate limit hit -- sleeping {}', pause)
             sleep(pause)                  # 5 s, then 10 s, then 15 s, etc.
+            if __debug__: log('doing recursive call #{}', recursing + 1)
             return net(get_or_post, url, session, polling, recursing + 1, **kwargs)
         error = RateLimitExceeded('Server blocking further requests due to rate limits')
     elif code == 503:
         error = ServiceFailure('Server is unavailable -- try again later')
     elif code in [500, 501, 502, 506, 507, 508]:
-        error = ServiceFailure('Server error (HTTP code {})'.format(code))
+        error = ServiceFailure(addurl('Server error (HTTP code {})'.format(code)))
     elif not (200 <= code < 400):
         error = NetworkFailure("Unable to resolve {}".format(url))
+    if __debug__: log('returning result {}',
+                      'with error {}'.format(error) if error else 'without error')
     return (req, error)
+
+
+# Next code originally from https://stackoverflow.com/a/39779918/743730
+def disable_ssl_cert_check():
+    requests.packages.urllib3.disable_warnings()
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
 
 
 def unwrapped_urllib3_exception(ex):
