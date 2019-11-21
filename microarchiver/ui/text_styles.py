bd33ea559668a9c305491cee912325ff4fded3ed
Michael Hucka <mhucka@caltech.edu>
2019-11-21 14:00:00 -0800

    Add new version of CLI & GUI classes
    This is another iteration of the message handler/CLI/GUI code I
    developed for many of these applications.  This time I've split it out
    into a separate subdirectory.
    
    The GUI part is not used in Microarchiver but I'm leaving it in for
    now, in case something changes in the future and we end up wanting to
    make it operational.

---
+++ microarchiver/ui/text_styles.py
@@ -0,0 +1,45 @@
+'''
+text_styles: color & style definitions for use with Python colorful.
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+import colorful
+
+# The following defines the basic styles we use in this application.
+# Regarding the ones like 'bold': There are more possible using the Python
+# colorful and other packages, but not all work on all platforms
+# (particularly Windows) and not all are that useful in pratice.  Note: the
+# following have to be defined after the palette above is loaded into
+# "colorful", or else the colors used below will not be defined when the
+# _COLORS dictionary is created at load time.
+
+colorful.update_palette({
+    'springGreen4'    : (  0, 139, 69),
+})
+
+_STYLES = {
+    'info'          : colorful.springGreen4,
+    'warn'          : colorful.orange,
+    'warning'       : colorful.orange,
+    'error'         : colorful.red,
+    'fatal'         : colorful.red & colorful.bold & colorful.underlined,
+
+    'blink'         : colorful.blinkslow,
+    'bold'          : colorful.bold,
+    'italic'        : colorful.italic,
+    'struckthrough' : colorful.struckthrough,
+    'underlined'    : colorful.underlined,
+}
+
+colorful.update_palette(_STYLES)
