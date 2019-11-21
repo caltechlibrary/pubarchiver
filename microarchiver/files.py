3ee96e0ded248e109d7b51502e1bc1b016eb82f4
Michael Hucka <mhucka@caltech.edu>
2019-11-21 10:01:10 -0800

    Use lates versions of some common utils & remove unused ones

--- microarchiver/files.py
+++ microarchiver/files.py
@@ -20,22 +20,12 @@ import shutil
 import subprocess
 import sys
 import tarfile
+import tempfile
 import webbrowser
 import zipfile
 from   zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
 
-import microarchiver
-from microarchiver.debug import log
-
-
-# Constants.
-# .............................................................................
-
-_APP_NAME = 'Microarchiver'
-'''The human name of this application for human consumption.'''
-
-_APP_REG_PATH = r'Software\Caltech Library\Microarchiver\Settings'
-'''The Windows registry path for this application.'''
+from .debug import log
 
 
 # Main functions.
@@ -48,48 +38,27 @@ def readable(dest):
 
 def writable(dest):
     '''Returns True if the destination is writable.'''
-    return os.access(dest, os.F_OK | os.W_OK)
 
-
-def module_path():
-    '''Returns the absolute path to our module installation directory.'''
-    # The path returned by module.__path__ is to the directory containing
-    # the __init__.py file.
-    return path.abspath(microarchiver.__path__[0])
-
-
-def installation_path():
-    '''Returns the path to where the application is installed.'''
-    # The path returned by module.__path__ is to the directory containing
-    # the __init__.py file.  What we want here is the path to the installation
-    # of the application binary.
-    if sys.platform.startswith('win'):
-        from winreg import OpenKey, CloseKey, QueryValueEx, HKEY_LOCAL_MACHINE, KEY_READ
+    # Helper function to test if a directory is writable.
+    def dir_writable(dir):
+        # This is based on the following Stack Overflow answer by user "zak":
+        # https://stackoverflow.com/a/25868839/743730
         try:
-            if __debug__: log('reading Windows registry entry')
-            key = OpenKey(HKEY_LOCAL_MACHINE, _APP_REG_PATH)
-            value, regtype = QueryValueEx(key, 'Path')
-            CloseKey(key)
-            if __debug__: log('path to windows installation: {}'.format(value))
-            return value
-        except WindowsError:
-            # Kind of a problem. Punt and return a default value.
-            return path.abspath('C:\Program Files\{}'.format(_APP_NAME))
+            testfile = tempfile.TemporaryFile(dir = dir)
+            testfile.close()
+        except (OSError, IOError) as e:
+            return False
+        return True
+
+    if path.exists(dest) and not path.isdir(dest):
+        # Path is an existing file.
+        return os.access(dest, os.F_OK | os.W_OK)
+    elif path.isdir(dest):
+        # Path itself is an existing directory.  Is it writable?
+        return dir_writable(dest)
     else:
-        return path.abspath(path.join(module_path(), '..'))
-
-
-def desktop_path():
-    '''Returns the path to the user's desktop directory.'''
-    if sys.platform.startswith('win'):
-        return path.join(path.join(os.environ['USERPROFILE']), 'Desktop')
-    else:
-        return path.join(path.join(path.expanduser('~')), 'Desktop')
-
-
-def datadir_path():
-    '''Returns the path to Lost It's internal data directory.'''
-    return path.join(module_path(), 'data')
+        # Path is a file but doesn't exist yet. Can we write to the parent dir?
+        return dir_writable(path.dirname(dest))
 
 
 def rename_existing(file):
@@ -118,25 +87,10 @@ def rename_existing(file):
         return
 
 
-def delete_existing(file):
-    '''Delete the given file.'''
-    # Check if it's actually a directory.
-    if path.isdir(file):
-        if __debug__: log('doing rmtree on directory {}', file)
-        try:
-            shutil.rmtree(file)
-        except:
-            if __debug__: log('unable to rmtree {}; will try renaming', file)
-            try:
-                rename_existing(file)
-            except:
-                if __debug__: log('unable to rmtree or rename {}', file)
-    else:
-        if __debug__: log('doing os.remove on file {}', file)
-        os.remove(file)
-
-
 def file_in_use(file):
+    '''Returns True if the given 'file' appears to be in use.  Note: this only
+    works on Windows, currently.
+    '''
     if not path.exists(file):
         return False
     if sys.platform.startswith('win'):
@@ -147,25 +101,6 @@ def file_in_use(file):
         except:
             return True
     return False
-
-
-def open_file(file):
-    '''Open document with default application in Python.'''
-    # Code originally from https://stackoverflow.com/a/435669/743730
-    if __debug__: log('opening file {}', file)
-    if sys.platform.startswith('darwin'):
-        subprocess.call(('open', file))
-    elif os.name == 'nt':
-        os.startfile(file)
-    elif os.name == 'posix':
-        subprocess.call(('xdg-open', file))
-
-
-def open_url(url):
-    '''Open the given 'url' in a web browser using the current platform's
-    default approach.'''
-    if __debug__: log('opening url {}', url)
-    webbrowser.open(url)
 
 
 def make_dir(dir_path):
