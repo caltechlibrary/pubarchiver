23b6806ba9f4256dbdd2b9a18f7bf600eb0f02e4
Michael Hucka <mhucka@caltech.edu>
2019-11-21 12:32:25 -0800

    Use updated version of my common __init__.py file

--- microarchiver/__init__.py
+++ microarchiver/__init__.py
@@ -21,17 +21,22 @@ import sys
 # Set module-level dunder variables like __version__
 # .............................................................................
 # The following code reads from either ../setup.cfg (if running from a source
-# directory) or the installed package metadata, and sets the corresponding
-# module-level variables with '__' surrounding their names.
+# directory) or data/setup.cfg (if running from an application created by
+# PyInstaller using our PyInstaller configuration scheme), or the installed
+# package metadata (if installed normally).  It reads config vars and sets the
+# corresponding module-level variables with '__' surrounding their names.
 
 keys = ['version', 'description', 'license', 'url', 'keywords',
         'author', 'author_email', 'maintainer', 'maintainer_email']
 
 this_module = sys.modules[__package__]
-setup_cfg = path.join(path.dirname(__file__), '..', 'setup.cfg')
+module_path = this_module.__path__[0]
+setup_cfg = path.join(module_path, 'data/setup.cfg')
+if not path.exists(setup_cfg):
+    setup_cfg = path.join(path.dirname(__file__), '..', 'setup.cfg')
 
 if path.exists(setup_cfg):
-    # If we are running from the source directory, we read from setup.cfg.
+    # If setup.cfg is directly available, use that.
     from setuptools.config import read_configuration
     conf_dict = read_configuration(setup_cfg)
     conf = conf_dict['metadata']
@@ -48,3 +53,14 @@ else:
     for name in [key for key in keys if hasattr(metadata, key)]:
         variable_name = '__' + name + '__'
         setattr(this_module, variable_name, getattr(metadata, name))
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    this_module = sys.modules[__package__]
+    print('{} version {}'.format(this_module.__name__, this_module.__version__))
+    print('Authors: {}'.format(this_module.__author__))
+    print('URL: {}'.format(this_module.__url__))
+    print('License: {}'.format(this_module.__license__))
